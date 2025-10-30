ad6afdacba9587dd9db835b8604f4cb901c46730
Emory Merryman <emory.merryman@gmail.com>
2025-10-30 11:19:27 -0400

    WTF

---
+++ flake.nix
@@ -0,0 +1,87 @@
+{
+    inputs = { } ;
+    outputs =
+        { self } :
+            {
+                lib =
+                    {
+                        coreutils ,
+                        git
+                    } :
+                        let
+                            implementation =
+                                {
+                                    configs ,
+                                    hooks ,
+                                    remotes ,
+                                    setup
+                                } :
+                                    {
+                                        init =
+                                            { resources , self } :
+                                                let
+                                                    init =
+                                                        writeShellApplication
+                                                            {
+                                                                name = "init" ;
+                                                                runtimeInputs = [ coreutils git ] ;
+                                                                text =
+                                                                    ''
+                                                                        mkdir --parents /mount/git-repository
+                                                                        cd /mount/git-repository
+                                                                        git init
+                                                                        ${ builtins.concatStringsSep "\n" ( builtins.attrValues ( builtins.mapAttrs ( name : value : ''git config "${ name }" "${ value }"'' ) configs ) ) }
+                                                                        ${ builtins.concatStringsSep "\n" ( builtins.attrValues ( builtins.mapAttrs ( name : value : ''ln --symbolic "${ value }" ".git/hooks/${ name }"'' ) hooks ) ) }
+                                                                        ${ builtins.concatStringsSep "\n" ( builtins.attrValues ( builtins.mapAttrs ( name : value : ''git remote add "${ name }" "${ value }"'' ) remotes ) ) }
+                                                                        ${ if builtins.typeOf setup == "null" then "#" else setup }
+                                                                    '' ;
+                                                            } ;
+                                                    in "${ application }/bin/init" ;
+                                        targets = [ "git-repository" ] ;
+                                    } ;
+                            in
+                                {
+                                    check =
+                                        {
+                                            config ? [ ] ,
+                                            expected ,
+                                            failure ,
+                                            hooks ? [ ] ,
+                                            mkDerivation ,
+                                            remotes ? [ ] ,
+                                            resources ? null ,
+                                            self ? null ,
+                                            setup ? null
+                                        } :
+                                            mkDerivation
+                                                {
+                                                    installPhase = ''execute-test "$out"'' ;
+                                                    name = "check" ;
+                                                    nativeBuildInputs =
+                                                        [
+                                                            (
+                                                                pkgs.writeShellApplication
+                                                                    {
+                                                                        name = "execute-test" ;
+                                                                        runtimeInputs = [ coreutils ( failure "b951ae86" ) ] ;
+                                                                        text =
+                                                                            let
+                                                                                init = instance.init { resources = resources ; self = self ; } ;
+                                                                                instance = implementation { configs = configs ; hooks = hooks ; remotes = remotes ; setup = setup ; } ;
+                                                                                in
+                                                                                    ''
+                                                                                        OUT="$1"
+                                                                                        touch "$OUT"
+                                                                                        ${ if [ init targets ] != builtins.attrNames instance then ''failure name "${ builtins.toJSON builtins.attrNames instance }"'' else "#" }
+                                                                                        ${ if [ "git-repository" ] != instance.targets then ''failure targets "${ builtins.toJSON instance.targets }"'' else "#" }
+                                                                                        ${ if init != expected then ''failure init "${ init }"'' else "" }
+                                                                                    '' ;
+                                                                    }
+                                                            )
+                                                        ] ;
+                                                    src = ./. ;
+                                                } ;
+                                    implementation = implementation ;
+                                } ;
+            } ;
+}
