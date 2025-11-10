9e024975fd53d52fb9fd8a848e1cb66df236c97e
Emory Merryman <emory.merryman@gmail.com>
2025-11-09 21:05:51 -0500

    git-repository

--- flake.nix
+++ flake.nix
@@ -61,7 +61,7 @@
                                                                                 ${ builtins.concatStringsSep "\n" ( builtins.attrValues ( config-visit ) ) }
                                                                                 ${ builtins.concatStringsSep "\n" ( builtins.attrValues ( builtins.mapAttrs ( name : value : ''ln --symbolic "${ value }" ".git/hooks/${ name }"'' ) hooks ) ) }
                                                                                 ${ builtins.concatStringsSep "\n" ( builtins.attrValues ( builtins.mapAttrs ( name : value : ''git remote add "${ name }" "${ value }"'' ) remotes ) ) }
-                                                                                ${ setup-visit }
+                                                                                ${ setup }
                                                                             '' ;
                                                             } ;
                                                     in "${ application }/bin/init" ;
