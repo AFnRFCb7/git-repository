87176c8afe9d1e679df34985850eaf45646d233c
Emory Merryman <emory.merryman@gmail.com>
2025-12-28 07:38:45 -0500

    abc

--- flake.nix
+++ flake.nix
@@ -51,13 +51,9 @@
                                                                             } ;
                                                                         in
                                                                             ''
-                                                                                echo 37d07eeb >&2
                                                                                 mkdir --parents /mount/repository
-                                                                                echo 5e0701f1 >&2
                                                                                 mkdir --parents /mount/stage
-                                                                                echo 06098ac6 >&2
                                                                                 cd /mount/repository
-                                                                                echo 6352e096 >&2
                                                                                 git init 2>&1
                                                                                 ${ builtins.concatStringsSep "\n" ( visitor visitors.setup setup ) }
                                                                             '' ;
